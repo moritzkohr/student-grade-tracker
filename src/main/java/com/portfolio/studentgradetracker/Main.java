f2a16ebe72c7e959bbce6efa1335cdc7414c6def
Moritz Kohr <morris.ko98@googlemail.com>
2025-10-09 10:56:37 +0200

    refactor: remove unused HelloApplication and HelloController
    - Deleted `hello-view.fxml`, `HelloApplication.java`, and `HelloController.java`
    - Renamed `Launcher` to `Main`
    - Updated `pom.xml` to reflect the renamed main class
    - Added TODO methods in `Main` for future implementation

---
+++ src/main/java/com/portfolio/studentgradetracker/Main.java
@@ -0,0 +1,24 @@
+//TODO: Implement
+package com.portfolio.studentgradetracker;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+
+    }
+
+    public void start(Stage primaryStage) {
+
+    }
+
+    public void setupPrimaryStage(Stage stage){
+
+    }
+
+
+    public void loadMainView(){
+
+    }
+}
